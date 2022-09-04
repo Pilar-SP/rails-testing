bb526ad96a8d2ef100a022f5b4a50c359b4a9fce
pilar sanchez <mpilar-93@hotmail.com>
2022-09-04 15:08:23 +0100

    created my first workflow

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,14 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "full_name returns the capitalized first name and last name" do
+    user = User.new(first_name: "john", last_name: "lennon")
+    assert_equal "John Lennon", user.full_name
+  end
+
+  test "first name returns first name" do
+    user = User.new(first_name: "Andy")
+    assert_equal "Andy", user.first_name
+  end
 end
